6d87b507e6e1cfea4b586d3a620d3b22eeaf27bc
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-09-10 16:51:18 +0700

    built files

---
+++ public/index.search.js
@@ -0,0 +1,130 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "3. Create Model Storage with S3",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "1. Create Gateway Endpoint",
+    "uri": "/3-create-model-storage-with-s3/1-create-gateway-endpoint/index.html"
+  },
+  {
+    "breadcrumb": "4. Build Server with Docker",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "1. Test Connection",
+    "uri": "/4-build-server-with-docker/1-test-connection/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "This is a new chapter.",
+    "description": "This is a new chapter.",
+    "tags": [],
+    "title": "VPC in AWS",
+    "uri": "/1-vpc-in-aws/index.html"
+  },
+  {
+    "breadcrumb": "VPC in AWS",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "1. VPC",
+    "uri": "/1-vpc-in-aws/1-vpc/index.html"
+  },
+  {
+    "breadcrumb": "3. Create Model Storage with S3",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "2. Upload Model to S3",
+    "uri": "/3-create-model-storage-with-s3/2-upload-model-to-s3/index.html"
+  },
+  {
+    "breadcrumb": "4. Build Server with Docker",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "2. Serving with Docker",
+    "uri": "/4-build-server-with-docker/2-serving-with-docker/index.html"
+  },
+  {
+    "breadcrumb": "VPC in AWS",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "2. Subnet",
+    "uri": "/1-vpc-in-aws/2-subnet/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "This is a new chapter.",
+    "description": "This is a new chapter.",
+    "tags": [],
+    "title": "2. Establish EC2 Server",
+    "uri": "/2-establish-ec2-server/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "This is a new chapter.",
+    "description": "This is a new chapter.",
+    "tags": [],
+    "title": "3. Create Model Storage with S3",
+    "uri": "/3-create-model-storage-with-s3/index.html"
+  },
+  {
+    "breadcrumb": "VPC in AWS",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "3. Route Table",
+    "uri": "/1-vpc-in-aws/3-route-table/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "This is a new chapter.",
+    "description": "This is a new chapter.",
+    "tags": [],
+    "title": "4. Build Server with Docker",
+    "uri": "/4-build-server-with-docker/index.html"
+  },
+  {
+    "breadcrumb": "VPC in AWS",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "4. Security Group",
+    "uri": "/1-vpc-in-aws/4-security-group/index.html"
+  },
+  {
+    "breadcrumb": "VPC in AWS",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "5. Internet Gateway",
+    "uri": "/1-vpc-in-aws/5-internet-gateway/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "This is a new chapter.",
+    "description": "This is a new chapter.",
+    "tags": [],
+    "title": "5. Clean Up",
+    "uri": "/5-clean-up/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
